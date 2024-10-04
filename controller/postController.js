f8f437c58f8c8dfb713e2fb262febc02eaf4ce39
Harshal <harshalpawar667@gmail.com>
2024-10-04 10:52:08 +0530

    Added

--- controller/postController.js
+++ controller/postController.js
@@ -186,8 +186,9 @@ exports.LikePost = async (req, res) => {
 
       // get UserSocketId //
 
-      const postOwnerSocketId = getReceiverSocketId(PostownerId)
-      io.to(postOwnerSocketId).emit('notification', notification)
+      // const postOwnerSocketId = getReceiverSocketId(PostownerId)
+      // console.log(postOwnerSocketId , "postOwnerSocketId")
+      // io.to(postOwnerSocketId).emit('notification', notification)
 
     }
 
@@ -238,9 +239,9 @@ exports.DisLike = async (req, res) => {
     
           }
     
-          // get UserSocketId //
-          const postOwnerSocketId = getReceiverSocketId(PostownerId)
-          io.to(postOwnerSocketId).emit('notification', notification)
+          // // get UserSocketId //
+          // const postOwnerSocketId = getReceiverSocketId(PostownerId)
+          // io.to(postOwnerSocketId).emit('notification', notification)
     
         }
 
