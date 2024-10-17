68f8e98a5a449141a55192b333c5c407c3f0874e
Harshal <harshalpawar667@gmail.com>
2024-10-17 12:57:26 +0530

    ADded

--- Socket/socket.js
+++ Socket/socket.js
@@ -10,7 +10,7 @@ const io = new Server(server, {
         origin: (origin, callback) => {
             // List of allowed origins
             const allowedOrigins = [
-                'http://3.110.28.248', // Frontend URL
+                'http://3.109.153.20', // Frontend URL
 
             ];
             
